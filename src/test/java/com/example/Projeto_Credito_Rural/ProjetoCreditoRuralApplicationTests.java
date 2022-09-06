18dd317542f85fd1a3da1108538bd3d0bacbde19
Guilherme de Oliveira <guilherme.oliver12@gmail.com>
2022-09-06 15:48:55 -0300

    criado repositório do projeto

---
+++ src/test/java/com/example/Projeto_Credito_Rural/ProjetoCreditoRuralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Projeto_Credito_Rural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCreditoRuralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
