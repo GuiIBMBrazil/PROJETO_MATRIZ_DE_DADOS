b06982d44ff64eef6f23127e5ce1881e6b3f60f8
Guilherme de Oliveira <guilherme.oliver12@gmail.com>
2022-09-06 16:34:01 -0300

    criado branch para realizar teste de consumo de API em formato JSON

--- src/main/java/com/example/Projeto_Credito_Rural/entity/Product.java
+++ src/main/java/com/example/Projeto_Credito_Rural/entity/Product.java
@@ -1,2 +1,29 @@
-package com.example.Projeto_Credito_Rural.entity;public class Product {
+package com.example.Projeto_Credito_Rural.entity;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nomeProduto;
+    private String nomeRegiao;
+    private String nomeUF;
+    private String MesEmissao;
+    private String AnoEmissao;
+    private String cdPrograma;
+    private String cdSubPrograma;
+    private String cdFonteRecurso;
+    private String cdTipoSeguro;
+    private Integer QtdCusteio;
+    private BigDecimal VlCusteio;
+    private String Atividade;
+    private String cdModalidade;
+    private String AreaCusteio;
 }
